2de98477b0e7fa9bc7aa4664a68044c9c4bb27a1
Dima Savkov <dimsaw85@gmail.com>
2021-11-29 15:02:33 +0200

    work1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
 
+let timerId = null;
+
+refs.startBtn.addEventListener('click', changeBackgroundColor);
+refs.stopBtn.addEventListener('click', () => {
+  clearInterval(timerId);
+  refs.startBtn.disabled = false;
+});
+
+function changeBackgroundColor() {
+  timerId = setInterval(() => {
+    const color = getRandomHexColor();
+    document.body.style.background = color;
+  }, 1000);
+  refs.startBtn.disabled = true;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
